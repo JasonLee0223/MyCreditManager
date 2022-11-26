cb3d465b40fb288a43cc64fc0a5b2135bef6261a
Jason <ggj00125@gmail.com>
2022-11-26 18:21:13 +0900

    Feat: SystemMenu Protocol 구현

---
+++ MyCreditManager/MyCreditManager/Protocol/SystemMenu.swift
@@ -0,0 +1,18 @@
+//
+//  SystemMenu.swift
+//  MyCreditManager
+//
+//  Created by Jason on 2022/11/26.
+//
+
+import Foundation
+
+protocol SystemMenu {
+    func menuStart()
+    func addToStudent()
+    func deleteToStudent()
+    func addOrChangeToGrade()
+    func deleteGrade()
+    func viewOfAverage()
+    func exitMenu()
+}
