341b961bae048d8292c76cdf9e7a3f0aba95aedd
Jason <ggj00125@gmail.com>
2022-11-26 22:48:32 +0900

    Feat: executeMenu 클래스를 생성하여 함수로 분기처리

--- MyCreditManager/MyCreditManager/main.swift
+++ MyCreditManager/MyCreditManager/main.swift
@@ -10,36 +10,36 @@ import Foundation
 class MyCreditManager {
     
     func menuStart() {
+        let executeMenu = ExecuteMenu()
         var exitMenu: String = ""
-
+        
         repeat {
             
             print("원하는 기능을 입력해주세요")
             print("1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
-
-            let inputValue = readLine()!//.map{ Int(String($0))!}
+            
+            let inputValue = readLine()!
             exitMenu = inputValue
             
             switch inputValue {
             case "1":
-//                addToStude()
+                executeMenu.addToStudent()
                 break
-
             case "2":
+                executeMenu.deleteToStudent()
                 break
-
             case "3":
                 break
-
+                
             case "4":
                 break
-
+                
             case "5":
                 break
-
+                
             case "X":
                 break
-
+                
             default:
                 print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
             }
@@ -48,9 +48,5 @@ class MyCreditManager {
     }
 }
 
-let temp = MyCreditManager()
-temp.menuStart()
-
-//func addToStudent() {
-//    print("추가할 학생의 이름을 입력해주세요")
-//}
+let startOfProgram = MyCreditManager()
+startOfProgram.menuStart()
